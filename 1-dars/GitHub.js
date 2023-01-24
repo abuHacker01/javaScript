fa73980b956e5aa94a3bdba2bdb635fece9c7fb9
abuhacker01 <abuhacker012@gmail.com>
2023-01-24 16:27:27 +0500

    oll works in 1-lesson

--- 1-dars/GitHub.js
+++ 1-dars/GitHub.js
@@ -10,16 +10,21 @@
 
 /// push to GitHub :
 
-// repository - project
-// 1 - git init
-// 2 - git remote add origin URL
+
+//// repositoryga ulash
+
+///// 1 - git init
+///// 2 - git remote add origin URL
+
 
 // birinchi urunishda xatolik ko'rsatadi u xolda quidagi ko'dlarni yozamiz :
 
 // 1. git config -global user.email "your@gmail.com"
 // 2. git config --global user.name "mukhtorov"
 
-// to push
-// 1. git add .
-// 2. git commit -m ""
-// 3. git push origin master
+
+// kiritish/yangilash
+
+/// 1. git add .
+/// 2. git commit -m ""
+/// 3. git push origin master
