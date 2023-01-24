8baed2d1f98a5f2b7a079a945d8e7bcd77d43029
abuhacker01 <abuhacker012@gmail.com>
2023-01-24 12:13:54 +0500

    this first lesson

---
+++ 1-dars/GitHub.js
@@ -0,0 +1,26 @@
+// GitHub :
+
+/// GitHub bu dasturchilar fleshkasi ,
+//// Yani ular GitHub orqali malumot ulashishadi .
+
+/// master.....................=> proectdagi eng katta branch (proectni o'zi) .
+/// brach......................=> yani bir kichik qisim .
+/// pull.......................=> ma'lum branch-dan ma'lum branch-ga ma'lumot yuborish .
+/// push.......................=> ma'lum branch-ni masterga qo'shish .
+
+/// push to GitHub :
+
+// repository - project
+// 1 - git init
+// 2 - git remote add origin URL
+// 2 - git remote add origin https://github.com/mukhtorov/g9.git
+
+// birinchi urunishda xatolik ko'rsatadi u xolda quidagi ko'dlarni yozamiz :
+
+// 1. git config -global user.email "your@gmail.com"
+// 2. git config --global user.name "mukhtorov"
+
+// to push
+// 1. git add .
+// 2. git commit -m ""
+// 3. git push origin master
