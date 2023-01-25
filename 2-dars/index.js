ac234055febef67e1c5817296f77984eea43d52f
abuhacker01 <abuhacker012@gmail.com>
2023-01-25 14:54:17 +0500

    data typs , varable

---
+++ 2-dars/index.js
@@ -0,0 +1,49 @@
+// varable
+
+/// varablelarning 3 turi bo'ladi :
+
+//// 1. VAR
+
+///// u oldindan beri bor bo'lgan.
+///// va u hoisting bo'ladi .
+///// uni qyta elon qilib bo'ladi .
+
+
+//// 2. LET
+
+///// U ES6 da qo'shilgan .
+///// uni qyta elon qilib bo'lmaydi .
+///// u hoisting bo'lmaydi .
+
+
+//// 3. CONST
+
+///// U ES6 da qo'shilgan .
+///// uni qyta elon qilib bo'lmaydi .
+///// u hoisting bo'lmaydi .
+///// uni elon qilganddan kegin o'zgartirib bo'lmaydi .
+
+// Data typs :
+
+/// Data type..............=> malumot turlari ;
+/// 8 ta data type bor.....=> 2 ga bolinadi ==> primitive vs non-primitive ;
+
+/// primetive : malum bir informatsiyalarni o'zida saqlaydi .
+/// non-primetive : bir 
+
+
+//// primitive :
+
+///// number...............=> bu son ;
+///// string...............=> "" , '' , `` , ichidagi hamma narsa string xissoblanadi ;
+///// boolean..............=> true, false ;
+///// null.................=> e'lon qilingan , va qiymatini  ;
+///// undefined............=> o'zgaruvchi e'lon qilingan ammo qiymat berilmagan ;
+///// bigInt...............=> on olti xonaligdan katta bo'lgan sonlar ;
+///// sybol................=> xech qacho takrorlanmas ID yaratib beradi ;
+
+////// null vs undefined : null-bu ob'ekt yo'qligining aniq qiymati, undefined esa noaniqlikni ;
+
+//// non-primitive :
+
+///// object...............=> array, function;
