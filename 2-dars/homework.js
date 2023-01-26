6386a58d3eb5bb8bdb86d70f0ea5e447547830c7
abuhacker01 <abuhacker012@gmail.com>
2023-01-26 17:57:18 +0500

    second lesson

---
+++ 2-dars/homework.js
@@ -0,0 +1,22 @@
+///////// 1.
+
+//// primitive :
+
+///// number...............=> bu son ;
+///// string...............=> "" , '' , `` , ichidagi hamma narsa string xissoblanadi ;
+///// boolean..............=> true, false ;
+///// null.................=> e'lon qilingan , va qiymatini  ;
+///// undefined............=> o'zgaruvchi e'lon qilingan ammo qiymat berilmagan ;
+///// bigInt...............=> on olti xonaligdan katta bo'lgan sonlar ;
+///// sybol................=> xech qacho takrorlanmas ID yaratib beradi ;
+
+
+//// non-primitive :
+
+///// object...............=> array, function;
+
+///////// 2.
+
+// console.log(`hello ${1}`);         // hello 1
+// console.log(`hello ${"name"}`);    // hello name
+// console.log(`hello ${name}`);      // hello Ilya
