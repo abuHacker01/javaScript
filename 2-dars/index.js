33ec2bcba5455b6a8764d8a8f7f4399ea9baf281
abuhacker01 <abuhacker012@gmail.com>
2023-01-26 17:25:11 +0500

    second lesson

--- 2-dars/index.js
+++ 2-dars/index.js
@@ -42,7 +42,13 @@
 ///// bigInt...............=> on olti xonaligdan katta bo'lgan sonlar ;
 ///// sybol................=> xech qacho takrorlanmas ID yaratib beradi ;
 
-////// null vs undefined : null-bu ob'ekt yo'qligining aniq qiymati, undefined esa noaniqlikni ;
+////// null vs undefined :
+
+/////// null-bu ob'ekt yo'qligining aniq qiymati, undefined esa noaniqlikni ;
+/////// null == undefined     // true
+/////// null === undefined    // false
+/////// isNaN(undefined)      // true
+/////// isNaN(null)           // false
 
 //// non-primitive :
 
