547147d3f86e53487a6e492272f44be5b33cdcf1
abuhacker01 <abuhacker012@gmail.com>
2023-01-27 18:35:57 +0500

    third lesson

---
+++ 3-dars/homeworke.js
@@ -0,0 +1,48 @@
+/////////// birinchi rasim
+
+// let a = 1,
+//     b = 1;
+// let c = ++a; // 2
+
+/// chunki agar o'zgaruvchi nomidan oldin "++" turgan bo'lsa o'zgaruvchiga 1-qo'shadi .
+
+// let d = b++; // 1
+
+/// chunki o'zgaruvchi nomidan kegin "++" turibdi .
+//// bu degani birinchi "b" ga tenglab kegin "b" ga 1-qoshyapti .
+
+// ================
+
+// let a = 2;
+// let x = 1 + (a *= 2)
+// console.log(x);     // x = 5
+
+/// chunki qavs ichida "a"ni "a" ko'paytirilgan 2 ga tengladik buning javobi esa 4 .
+/// qavs taqisida 4-ga 1 qo'shilgan uni javobiesa : 5
+
+// ================
+
+// console.log("" + 1 + 0);       // stringda 10
+/// chunki birinchi o'rinda stringga 1-ni qo'shyapmiz javob stringda 1
+/// kegin stringda 1-ga 0ni qo'shyapmiz javob (--stringda 10--)
+
+// console.log("" - 1 + 0);       // -1 numberda
+/// birinchi bo'lib stringdan 1-ni ayrildi javob "-1"
+/// kegin "-1"-ga 0-ni qo'shamiz o'z xolicha qoladi ;
+
+// true + false     // 1 numberda
+// 6 / "3"          // 2 numberda
+// "2" * "3"        // 6 numberda
+// 4 + 5 + "px"     // 9px stringda
+// "$" + 5 + 4      // $54 stringda
+// "4" - 2          // 2 numberda
+// "4px" - 2        // NaN
+// " -9 " + 5       // " -9 5 " stringda
+// " -9 " - 5       // -14 numberda
+// null + 1         // 1 numberda
+// undefined + 1    // NaN
+// "\t \n" - 2      // -2 numberda
+
+
+
+/////////// ikkinchi rasim
