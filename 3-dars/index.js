cae8f4d31b0cad2a8ba786120cc80addebc090c4
abuhacker01 <abuhacker012@gmail.com>
2023-01-28 18:06:58 +0500

    third lesson work

---
+++ 3-dars/index.js
@@ -0,0 +1,16 @@
+// Type Convirsion
+
+/// Type Convirsion-bu ma'lum data turidagi ma'lumotni boshqa data turiga o'zgartirish\
+
+/// BOOLEAN ==> STRING
+
+// first
+
+// let valeu = true;     // yangi o'zgaruvchhi ochildi ;
+// console.log(typeof valeu); // bu yerda boolean chiqadi ;
+// valeu = String(valeu);  // shu yerda stringga o'zgaryapti ;
+// console.log(typeof valeu);  // string-deb chiqadi ;
+
+
+// second
+
