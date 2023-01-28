35949f185f9185adeb7616ebde089aaff2d522d6
abuhacker01 <abuhacker012@gmail.com>
2023-01-28 19:57:44 +0500

    third lesson work

--- 3-dars/index.js
+++ 3-dars/index.js
@@ -12,14 +12,12 @@
 // console.log(typeof valeu);  // string-deb chiqadi ;
 
 
-// second
+// STRIN ==> NUMBER
 
 // let str = "123";
 // console.log(typeof str);    // string ;
 // str = Number(str);          // shu yerda Numberga o'tdi ;
 // console.log(typeof str);    // Number chiqadi ;
-
-// third
 
 /// agar string ustida (+)-dan tashqari barcha arifmetik amal bajarsaxam Numberga o'tadi
 
@@ -27,4 +25,35 @@
 // console.log("2" * "2");          |____ bu xolatlarda
 // console.log("2" - "2");          |
 
+/// agar stringni oldiga (+) qo'yib qo'ysaxam Numberga o'tadi ;
+
 /// agar hariflarni ustida arifmetik amal bajarsak NaN chiqadi ;
+
+// Basic operators , maths :
+
+/// 1. +.......................=> qo'shuv
+/// 2. -.......................=> ayruv
+/// 3. *.......................=> ko'paytiruv
+/// 4. /.......................=> bo'luv
+/// 5. %.......................=> foizlig bo'luv
+/// 6. **......................=> kvadrat
+
+// &&..........................=> (--va--) belgisi :
+
+/// bu belgi bir tomonidaxam false bo'lsa (false) qaytaradi ;
+/// faqat ikkala tomonida true bo'lsa kegin (true) qaytaradi ;
+
+
+// ||..........................=> (--yoki--) belgisi :
+
+/// bu belgi bir tomonidaxam true bo'lsa (true) qaytaradi ;
+/// faqat ikkala tomonida false bo'lsa kegin (false) qaytaradi ;
+
+// ==..........................=> teng yoki yo'qligini aytadi .
+
+/// misol uchun :
+
+//// console.log("1" == 1);      // true
+
+// ===.........................=> qat'iy tenglig :
+/// uni oddiy tenglikdan farqi
