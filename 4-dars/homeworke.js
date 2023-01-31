e688885371c814b6c069964ce1a5fb7a03321c91
abuhacker01 <abuhacker012@gmail.com>
2023-01-31 17:46:56 +0500

    homework in forth lesson

---
+++ 4-dars/homeworke.js
@@ -0,0 +1,11 @@
+// if ("0") {                       |
+//     console.log("Hello");        |---------bu yerda xech nima chiqmaydi ;
+// }                                |
+
+let javob = "I dont know";
+
+if (javob == "ECMAScript") {
+    console.log("Right!");
+} else {
+    console.log(`You don't know?\n "ECMAScript"`);
+}
