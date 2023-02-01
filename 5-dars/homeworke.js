975fdbf422e3f3ef7c83f958a4c0cf9b806dcbf2
abuhacker01 <abuhacker012@gmail.com>
2023-02-01 14:08:55 +0500

    fifth lesson

---
+++ 5-dars/homeworke.js
@@ -0,0 +1,37 @@
+let num = 1;
+let res = "";
+switch (num) {
+    case [1] == 1:
+        res = res + "bir yuz";
+        break;
+    case [1] == 2:
+        res = res + "ikki yuz";
+        break;
+    case [1] == 3:
+        res = res + "uch yuz";
+        break;
+    case [1] == 4:
+        res = res + "tor yuz";
+        break;
+    case [1] == 5:
+        res = res + "besh yuz";
+        break;
+    case [1] == 6:
+        res = res + "olti yuz";
+        break;
+    case [1] == 7:
+        res = res + "yetti yuz";
+        break;
+    case [1] == 8:
+        res = res + "sakkiz yuz";
+        break;
+    case [1] == 9:
+        res = res + "to'qqiz yuz";
+        break;
+
+    default:
+
+        break;
+}
+
+console.log(res);
