1bc382eaa9264671cb44a79c2d999681fa8d4b3a
abuhacker01 <abuhacker012@gmail.com>
2023-02-02 14:54:30 +0500

    homeworks

--- 5-dars/homeworke.js
+++ 5-dars/homeworke.js
@@ -1,96 +1,107 @@
-let num = Math.round(Math.random() * 1000);
-console.log(num);
-let yuzlig = parseInt(num / 100);
-num = num % 100;
-let onlig = parseInt(num / 10);
-num = num % 10;
-let birlig = num;
-a = "", b = "", d = "";
-switch (yuzlig) {
-    case 1:
-        a = "bir yuz";
-        break;
-    case 2:
-        a = "ikki yuz";
-        break;
-    case 3:
-        a = "uch yuz";
-        break;
-    case 4:
-        a = "to'r yuz";
-        break;
-    case 5:
-        a = "bech yuz";
-        break;
-    case 6:
-        a = "olti yuz";
-        break;
-    case 7:
-        a = "yetti yuz";
-        break;
-    case 8:
-        a = "sakkiz yuz";
-        break;
-    case 9:
-        a = "to'qqiz yuz";
-        break;
-}
-switch (onlig) {
-    case 1:
-        b = "o'n";
-        break;
-    case 2:
-        b = "yigirma";
-        break;
-    case 3:
-        b = "o'ttiz";
-        break;
-    case 4:
-        b = "qiriq";
-        break;
-    case 5:
-        b = "ellig";
-        break;
-    case 6:
-        b = "oltmush";
-        break;
-    case 7:
-        b = "yetmush";
-        break;
-    case 8:
-        b = "sakson";
-        break;
-    case 9:
-        b = "to'qson";
-        break;
-}
-switch (birlig) {
-    case 1:
-        c = "bir";
-        break;
-    case 2:
-        c = "ikki";
-        break;
-    case 3:
-        c = "uch";
-        break;
-    case 4:
-        c = "to'rt";
-        break;
-    case 5:
-        c = "bech";
-        break;
-    case 6:
-        c = "olti";
-        break;
-    case 7:
-        c = "yetti";
-        break;
-    case 8:
-        c = "sakkiz";
-        break;
-    case 9:
-        c = "to'qqiz";
-        break;
-}
-console.log(a, b, c);
+////////////////////// raqamlarni xarifga o'tkazish
+
+// let num = Math.round(Math.random() * 1000);
+// console.log(num);
+// let yuzlig = parseInt(num / 100);
+// num = num % 100;
+// let onlig = parseInt(num / 10);
+// num = num % 10;
+// let birlig = num;
+// a = "", b = "", d = "";
+// switch (yuzlig) {
+//     case 1:
+//         a = "bir yuz";
+//         break;
+//     case 2:
+//         a = "ikki yuz";
+//         break;
+//     case 3:
+//         a = "uch yuz";
+//         break;
+//     case 4:
+//         a = "to'r yuz";
+//         break;
+//     case 5:
+//         a = "bech yuz";
+//         break;
+//     case 6:
+//         a = "olti yuz";
+//         break;
+//     case 7:
+//         a = "yetti yuz";
+//         break;
+//     case 8:
+//         a = "sakkiz yuz";
+//         break;
+//     case 9:
+//         a = "to'qqiz yuz";
+//         break;
+// }
+// switch (onlig) {
+//     case 1:
+//         b = "o'n";
+//         break;
+//     case 2:
+//         b = "yigirma";
+//         break;
+//     case 3:
+//         b = "o'ttiz";
+//         break;
+//     case 4:
+//         b = "qiriq";
+//         break;
+//     case 5:
+//         b = "ellig";
+//         break;
+//     case 6:
+//         b = "oltmush";
+//         break;
+//     case 7:
+//         b = "yetmush";
+//         break;
+//     case 8:
+//         b = "sakson";
+//         break;
+//     case 9:
+//         b = "to'qson";
+//         break;
+// }
+// switch (birlig) {
+//     case 1:
+//         c = "bir";
+//         break;
+//     case 2:
+//         c = "ikki";
+//         break;
+//     case 3:
+//         c = "uch";
+//         break;
+//     case 4:
+//         c = "to'rt";
+//         break;
+//     case 5:
+//         c = "bech";
+//         break;
+//     case 6:
+//         c = "olti";
+//         break;
+//     case 7:
+//         c = "yetti";
+//         break;
+//     case 8:
+//         c = "sakkiz";
+//         break;
+//     case 9:
+//         c = "to'qqiz";
+//         break;
+// }
+// console.log(a, b, c);
+
+////////////////////// yup sonlar
+
+// let c = 1;
+// for (let num = 2; num <= 100; num++) {
+//     for (let i = 1; i <= num; i++) if (num % i === 0) c++;
+//     if (c == 2) console.log(num);
+//     c = 0;
+// }
