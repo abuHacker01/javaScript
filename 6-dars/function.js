9596ea3d6d8b92d42bbac3ca710753c3f748b962
abuhacker01 <abuhacker012@gmail.com>
2023-02-04 19:22:25 +0500

    work on lesson 6

---
+++ 6-dars/function.js
@@ -0,0 +1,24 @@
+
+// Function :
+
+/// Function bu javascriptdagi dastur kod bloci ;
+
+/// Hoisting :
+
+//// functionlar hoisting bo'ladi .
+//// chunki javascript engine yurganda birinchi bo'lib function keyvordlarini o'qib oladi ;
+
+/// call :
+
+//// call - bu Functionimizni chaqirish ;
+//// call - agar Functionimizni chaqirmasak u ishlamaydi ;
+
+/// return :
+
+//// returnni yonida nima turgan bo'lsa osha functionni qiymati bo'ladi ;
+//// return function-ni eng oxirida yoziladi va undan kegin yozilgan xar qanday kod ishlamaydi ;
+
+// {
+/// scope :
+//// block :
+// }
