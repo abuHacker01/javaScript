145ccd1c2494b6f0a71572a7b71aed9ac9be7504
abuhacker01 <abuhacker012@gmail.com>
2023-02-04 19:38:26 +0500

    work on lesson 6

--- 6-dars/function.js
+++ 6-dars/function.js
@@ -3,7 +3,7 @@
 
 /// Function bu javascriptdagi dastur kod bloci ;
 
-/// Hoisting :
+/// Dinamic function :
 
 //// functionlar hoisting bo'ladi .
 //// chunki javascript engine yurganda birinchi bo'lib function keyvordlarini o'qib oladi ;
@@ -13,12 +13,14 @@
 //// call - bu Functionimizni chaqirish ;
 //// call - agar Functionimizni chaqirmasak u ishlamaydi ;
 
+/// let/const o'z hususiyatini yo'qotishi :
+
+//// agar globalda bitta let orqali o'zgaruvchi ochib va functionni ichidaxam
+///// globaldagi bilan name-si bir xil verable ochsak xato ko'rsatmaydi ;
+
+////// va functionni chaqirsag functionni ichidagi verable chiqadi ;
+
 /// return :
 
 //// returnni yonida nima turgan bo'lsa osha functionni qiymati bo'ladi ;
 //// return function-ni eng oxirida yoziladi va undan kegin yozilgan xar qanday kod ishlamaydi ;
-
-// {
-/// scope :
-//// block :
-// }
