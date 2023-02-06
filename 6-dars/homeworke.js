556b5185e512886b4456f75c3c65ada58d76affa
abuhacker01 <abuhacker012@gmail.com>
2023-02-06 16:17:48 +0500

    functions

---
+++ 6-dars/homeworke.js
@@ -0,0 +1,61 @@
+// to binry :
+
+/// let num = 3;
+/// console.log(num.toString(2));
+
+// chanje while
+
+// for (let i = 0; i < 3; i++) {
+//     console.log(`number ${i}!`);
+// }
+// let i = 0
+// while (i < 3) {
+// i++
+//     console.log(`number ${i}!`);
+// }
+
+// 18+
+
+// function chackAge(age) {
+//     if (age > 18) {
+//         return console.log(true);
+//     } else {
+//         return console.log('Did parents allow you?');
+
+//     }
+// }
+// chackAge(19);
+
+// ? and ||
+
+// function chackAge(age) {
+//     if (age || 18) {
+//         return console.log(true);
+//     }else{
+
+//        return console.log('Did parents allow you?');
+//     }
+// }
+
+// chackAge(19)
+
+// a ><= b
+
+// function kk(a, b) {
+
+//     if (a < b) {
+//         return console.log(a);
+//     }
+//     else {
+//         return console.log(b);
+//         }
+// }
+// kk(1, -3);
+
+// karra-karra
+
+// function x(a, b) {
+//     return console.log(a ** b);
+// }
+// x(3, 3);
+
