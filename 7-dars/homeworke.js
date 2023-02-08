a65ab12772af868abba1f83a51c30eb2fdce7662
abuhacker01 <abuhacker012@gmail.com>
2023-02-08 20:23:05 +0500

    works

--- 7-dars/homeworke.js
+++ 7-dars/homeworke.js
@@ -19,12 +19,13 @@
 /// console.log(isEmpty(user))
 
 // eng katta oylig
- 
+
 /// let obj = {
-///     Joxn: 100,
-///     Ann: 160,
-///     Pete: 1300
+///     Ann: Math.round(Math.random() * 1000),
+///     Joxn: Math.round(Math.random() * 1000),
+///     Pete: Math.round(Math.random() * 1000)
 /// };
+/// console.log(obj);
 /// let valeus = Object.values(obj);
 /// let res = 0;
 /// if (valeus[0] > res) {
@@ -32,6 +33,24 @@
 /// } if (valeus[1] > res) {
 ///     res = valeus[1];
 /// } if (valeus[2] > res) {
+///     res = valeus[2];
+/// }
+/// console.log(res);
+
+/// if (valeus[0] > valeus[1] && valeus[0] < valeus[2] || valeus[0] < valeus[1] && valeus[0] > valeus[2]) {
+///     res = valeus[0];
+/// } if (valeus[1] > valeus[0] && valeus[1] < valeus[2] || valeus[1] < valeus[0] && valeus[1] > valeus[2]) {
+///     res = valeus[1];
+/// } if (valeus[2] > valeus[1] && valeus[2] < valeus[0] || valeus[2] < valeus[1] && valeus[2] > valeus[0]) {
+///     res = valeus[2];
+/// }
+/// console.log(res);
+
+/// if (valeus[0] < res) {
+///     res = valeus[0];
+/// } if (valeus[1] < res) {
+///     res = valeus[1];
+/// } if (valeus[2] < res) {
 ///     res = valeus[2];
 /// }
 /// console.log(res);
