59d321ae504b65f12230a58f4129bd62a790a5b2
abuhacker01 <abuhacker012@gmail.com>
2023-02-08 18:45:55 +0500

    works

--- 7-dars/homeworke.js
+++ 7-dars/homeworke.js
@@ -1,5 +1,19 @@
-// let user = {};
-// user.name = "John";
-// user.surname = "Smith";
-// user.name = "Pate"
-// delete user.name;
+// mandarinni axlatga tashlash
+
+/// let user = {};
+/// user.name = "John";
+/// user.surname = "Smith";
+/// user.name = "Pate"
+/// delete user.name;
+
+// objectni bo'sh-bo'shemasligini tekshirib beradi
+
+/// function isEmpty(test) {
+///     if (test === {}) {
+///         return true;
+///     } else {
+///         return false;
+///     }
+/// }
+
+/// console.log(isEmpty(user))
