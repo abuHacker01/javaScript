e8819e6177b79da00c83afa8737370fa0737f527
abuhacker01 <abuhacker012@gmail.com>
2023-02-08 19:54:41 +0500

    works

--- 7-dars/homeworke.js
+++ 7-dars/homeworke.js
@@ -17,3 +17,21 @@
 /// }
 
 /// console.log(isEmpty(user))
+
+// eng katta oylig
+ 
+/// let obj = {
+///     Joxn: 100,
+///     Ann: 160,
+///     Pete: 1300
+/// };
+/// let valeus = Object.values(obj);
+/// let res = 0;
+/// if (valeus[0] > res) {
+///     res = valeus[0];
+/// } if (valeus[1] > res) {
+///     res = valeus[1];
+/// } if (valeus[2] > res) {
+///     res = valeus[2];
+/// }
+/// console.log(res);
