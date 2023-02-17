4957f8e398e3fa6a28ebd611c7f8e111069e0722
abuhacker01 <abuhacker012@gmail.com>
2023-02-17 15:27:32 +0500

    works

--- 7-dars/object.js
+++ 7-dars/object.js
@@ -36,4 +36,42 @@
 //     surname: "Xabibullaev",
 //     age: 15
 // };
+// let copyinfoD = structuredClone(infoD);// this 
+
+/// this bu thini o'rab turgan functionni o'rab turgan object
+//// misol uchun :
+
+// let object = {
+//     name: "Dilmurod",
+//     surname: "Xabibullaev",
+//     fn() {
+//         console.log(this.name + this.surnam);
+//     }
+// };
+// console.log(object.fn);
+
+/// qachon this kerak ?
+//// qachon oddiy usulda korsatsak keginchalik objectni nomini o'zgartirsak bizning usul ishlamaydi ;
+
+/// qachon this ishlamaydi ?
+
+//// qachonki arrow function ishlasa
+
+
+// garberge collaction :
+
+/// garberge collectio b qachonki non-primetive data turilig verable yaratsag
+//// va uni primetive-ga o'tkazsag stackdagi malumot bilan heapdagi malumoni bog'lab turuvchi ko'prig uzuladi va heapdagi malumot garbergega tushadi ;
+
+// structuredClone :
+
+/// structuredClone bu objectni faqat qiymatlarini copy qilish ;
+
+/// syntax of structuredClone :
+
+// let infoD = {
+//     name: "dilmurod",
+//     surname: "Xabibullaev",
+//     age: 15
+// };
 // let copyinfoD = structuredClone(infoD);
