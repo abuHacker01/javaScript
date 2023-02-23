be34f1755b8df8b78512d946339a9201171f74d2
abuhacker01 <abuhacker012@gmail.com>
2023-02-23 15:49:59 +0500

    works

--- 8-dars/homework.js
+++ 8-dars/homework.js
@@ -1,23 +1,19 @@
-let string = "webbrain";
-function name(str) {
-    let res = {};
-    for (l of str) {
-        res[l] ? res[l] = res[l] + 1 : res[l] = 1;
-    }
-    console.log(res);
-}
-name(string);
+// let string = "DilmurodXabibullaev";
+// function name(str) {
+//     let res = {};
+//     for (l of str) {
+//         res[l] ? res[l] = res[l] + 1 : res[l] = 1;
+//     }
+//     console.log(res);
+// }
+// name(string);
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+// let str = "opiafoduify3123#!@#!dsjascnknnalhdksckah";
+// let res = { string: 0, number: 0, char: 0 };
+// for (l of str) {
+//     let lt = l.toLowerCase();
+//     if (!isNaN(l)) res.number++;
+//     else if (lt !== l.toUpperCase()) res.string++;
+//     else res.char++;
+// }
+// console.log(res);
--- 8-dars/number str.js
+++
@@ -1,9 +0,0 @@
-let str = "opiafoduify3123#!@#!dsjascnknnalhdksckah";
-let res = { string: 0, number: 0, char: 0 };
-for (l of str) {
-    let lt = l.toLowerCase();
-    if (!isNaN(l)) res.number++;
-    else if (lt !== l.toUpperCase()) res.string++;
-    else res.char++;
-}
-console.log(res);
