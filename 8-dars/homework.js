c94326ea3227566804adf0f8685ffc61614cc847
abuhacker01 <abuhacker012@gmail.com>
2023-02-23 12:42:37 +0500

    work

--- 8-dars/homework.js
+++ 8-dars/homework.js
@@ -1,23 +1,23 @@
-// function name(str) {
-//     let res = {};
-//     for (l of str) {
-//         if (res[l]) {
-//             res[l] = res[l] + 1;
-//         } else {
-//             res[l] = 1;
-//         }
-//     }
-//     console.log(res);
-// }
-// name("Webbrain Academy");
+let string = "webbrain";
+function name(str) {
+    let res = {};
+    for (l of str) {
+        res[l] ? res[l] = res[l] + 1 : res[l] = 1;
+    }
+    console.log(res);
+}
+name(string);
 
 
-// let str = "opiafoduify3123#!@#!dsjascnknnalhdksckah";
-// let res = { number: 0, string: 0, char: 0 };
-// for (l of str) {
-//     let lt = l.toLowerCase();
-//     if (!isNaN(l)) res.number++;
-//     else if (lt !== l.toUpperCase()) res.string++;
-//     else res.char++;
-// }
-// console.log(res);
+
+
+
+
+
+
+
+
+
+
+
+
