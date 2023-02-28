62b88fa111300fbd79043754297c7b553989ab1f
abuhacker01 <abuhacker012@gmail.com>
2023-02-28 20:19:00 +0500

    works

--- 9-dars/homework.js
+++ 9-dars/homework.js
@@ -1,9 +1,74 @@
-let arr = [9, 1, 2, 3, 4, 5, 6];
-let res = arr.sort((a, b) => a - b);
-res = arr.slice(-2);
-res = arr = arr.sort((a, b) => b - a);
-let res1 = res.splice(0, 1);
-let res2 = res.splice(1);
-console.log(res1 - res2);
+// let arr = [5, 4, 3, 9];
+// console.log(eval(arr.join("+")));
 
-console.log(arr);
+
+// let arr = ["Webbrain", "academy"];
+// function name(str) {
+//     let res = {};
+//     for (l of str) {
+//         if (res[l]) res[l] = res[l] + 1; else res[l] = 1;
+//     }
+//     console.log(res);
+// }
+// name(arr + "");
+
+
+// let arr1 = ["hello"];
+// let arr2 = ["hello"];
+// function arrTaq(arr1, arr2) {
+//    arr1 = arr1 + "";
+//    arr2 = arr2 + "";
+//     console.log(arr1 == arr2);
+// }
+// arrTaq(arr1,arr2)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let users = [
+//     {id:1, year: 1998, engine: 1, name: "Tico"},
+//     {id:2, year: 2005, engine: 1.2, name: "Matiz"},
+//     {id:3, year: 2010, engine: 1.6, name: "Gentra"},
+//     {id:4, year: 2010, engine: 1.5, name: "Cobalt"},
+//     {id:5, year: 2012, engine: 2, name: "Malibu"},
+//     {id:6, year: 2000, engine: 1.2, name: "Damas"},
+//     {id:7, year: 2018, engine: 2.4, name: "Tracer"}
+// ]
+
+// let res1 = users.filter((v)=> v.year < 2000)
+// console.log(res1);
+
+
+// let res2 = users.filter((v)=> v.year > 2010)
+// console.log(res2);
+
+
+// let res3 = users.sort((a, b) => a.engine - b.engine )
+// console.log(res3);
+
+
+// let res4 = users.sort((a, b) => a.year - b.year )
+// console.log(res4);
+
+
+// let res5 = users.sort((a, b) => a.name.localeCompare(b.name) )
+// console.log(res5);
+
+// let res6 = users.filter((v)=> v.year < 2000)
+// console.log(res6);
