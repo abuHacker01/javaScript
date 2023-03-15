3cd4c2c7e6833f7d50e067f69ccdc62561b1626f
abuhacker01 <abuhacker012@gmail.com>
2023-03-15 18:37:24 +0500

    works

--- 8-dars/homework.js
+++ 8-dars/homework.js
@@ -22,15 +22,7 @@
 // string ichidagi malumotlardan nechtaligini aytadi :
 
 
-// let string = "DilmurodXabibullaev";
-// function name(str) {
-//     let res = {};
-//     for (l of str) {
-//         if (res[l]) res[l] = res[l] + 1; else res[l] = 1;
-//     }
-//     console.log(res);
-// }
-// name(string);
+\
 
 
 
