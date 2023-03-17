3894671a49636d10ba52f5ca8d032799ca8d7581
abuhacker01 <abuhacker012@gmail.com>
2023-03-17 19:46:49 +0500

    works

--- 16-module/homework.js
+++ 16-module/homework.js
@@ -1,20 +1,18 @@
 let arr = [
     { id: 1, name: "Dilmurod", status: "№1" }
 ];
-let obj = new Object;
-let i = 1;
 
-function add(arr) {
-    let obj = (obj) => {
-        i += 1;
-        obj += obj.id = i;
-        obj += obj.name = "eshmat";
-        obj += obj.status = `№${i}`;
-        let gg = obj;
-    };
-    arr.push(gg);
-}
-add(arr);
-add(arr);
-add(arr);
+let add = (id) => {
+    let obj = {};
+    obj.id = id;
+    obj.name = "eshmat";
+    obj.status = `№${id}`;
+    arr.push(obj);
+};
+
+add(2);
+add(98765)
+
+
+
 console.log(arr);
