4d28c4b271396e8fbd0911d6fa55a6187bdc395e
abuhacker01 <abuhacker012@gmail.com>
2023-03-27 00:07:10 +0500

    works

--- 17-dars/try catch, collback.js
+++ 17-dars/try catch, collback.js
@@ -1,6 +1,35 @@
+// Error handling :
+
+/// bizda biron-bir muammo bo'ladigan bo'lsa proeckt to'gridan-to'g'ri ishlashdan to'xtaydi ;
+//// uni oldini olish uchun (--try catch--)-dan foidalanish
+
+/// syntax of try catch :
+
+// let str = "dadaadadadadadadada";
 // try {
-//     console.log(jj);
-//     throw new Error(`loy`);
-// } catch (error) {
-//     console.log(error.message);
+//     console.log(num);
+// } catch {
+//     console.log(str);
 // }
+
+//// console.log(num)-qilganimizda (num is not defined)-chiqadi . Lekin catch-da (console.log(str);)-deb yozganmiz uchun str-ning qiymati chiqadi ;
+
+//// biz catch-ga "condition" berishinmiz mumkun . Uni keginchalig trydagi kelgan errorni nazarda tutsag bo'ladi misol uchn :
+
+// try {                                        |
+//     console.log(num);                        |
+// } catch (err) {                              |------------------ bu yerda (--ReferenceError: num is not defined--)-chiqadi ;
+//     console.log(err);                        |
+// }                                            |
+
+// try {                                        |
+//     console.log(num);                        |
+// } catch (err) {                              |--------- bu yerda (--ReferenceError--)-chiqadi ;
+//     console.log(err.name);                   |
+// }                                            |
+
+// try {                                        |
+//     console.log(num);                        |
+// } catch (err) {                              |--------- bu yerda (--num is not defined--)-chiqadi ;
+//     console.log(err.name);                   |
+// }                                            |
