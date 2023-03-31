e9ddffb08f5226816feec563d2002944d6225942
abuhacker01 <abuhacker012@gmail.com>
2023-03-31 09:17:55 +0500

    works

---
+++ 20-dars/DOM2.js
@@ -0,0 +1,34 @@
+// DOM 2
+
+/// agar biz html-ga biron-bi element qo'shmoxchi bo'lsag :
+
+//// let body = document.querySelector(".theBiggest");                   |
+//// body.before(`<h1>before</h1>`)                                      |                                           <h1>before</h1>
+//// body.append(`<h1>append</h1>`);                                     |-----------------bu yeda bu narsa chiqadi: <h1>prepend</h1> <h1>append</h1>
+//// body.prepend(`<h1>prepend</h1>`)                                    |                                           <h1>after</h1>
+//// body.after(`<h1>after</h1>`);                                       |
+
+///// before.............=> biz ko'rsatkan elementdan oldin element qo'shadi ;
+///// append.............=> biz ko'rsatkan elementni boshidan element qo'shadi ;
+///// prepend............=> biz ko'rsatkan elementni oxiridan element qo'shadi ;
+///// after..............=> biz ko'rsatkan elementdan kegin elementdan qo'shadi ;
+
+/// remove :
+
+//// biz agar body.remove() dydigan bo'lsak ko'rsatgan elementimiz o'chib ketadi ;
+
+/// cloneNode :
+
+//// cloneNode-bizga ko'rsatgan elementimizni ichidagi yozuvda tashqari xama narsanini copy qilib beradi ;
+
+///// let body = document.querySelector(".theBiggest");                        |__________ bu yerda body-dan clonega clone qilyapti ;
+///// let clone = body.cloneNode()                                             |
+
+
+/// scrole :
+
+//// scroleni uchta turi bor :
+
+///// 1. scroll
+///// 2. scrolto
+///// 3.
