492426d75cbb8ec478ef6706f4e68621a8cb957b
abuhacker01 <abuhacker012@gmail.com>
2023-04-01 21:12:18 +0500

    works

--- 20-dars/homework/index.js
+++ 20-dars/homework/index.js
@@ -3,8 +3,5 @@ let ball = document.querySelector("#ball");
 
 box.addEventListener("mousemove", (event) => {
     ball.style.marginTop = `${event.y - 55}px`;
-    if (event.y > 400 && event.y > 540 && event.x > 10 && event.x < 36) {
-        alert(`SUUUUUUUUUUUUUUUUUUUUUUUUUUUUIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIII
-    }
     ball.style.marginLeft = `${event.x - 65}px`;
 });
