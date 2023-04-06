300d491ede97b2ad101d2128e49270a2541cab1a
abuhacker01 <abuhacker012@gmail.com>
2023-04-06 20:20:11 +0500

    works

---
+++ rtworks/1-dars/index.js
@@ -0,0 +1,5 @@
+// React
+/// React - bu JavaScript kutubxonasidir.
+
+/// nega ractni - (--single page application--) deyishadi :
+//// chunki butun boshli react papkasida faqat bitta HTML- fayl bo'ladi
