15ba5af9a7d46c7961f6b94032ebec0bfb32af32
abuhacker01 <abuhacker012@gmail.com>
2023-04-21 15:34:38 +0500

    works

--- rtworks/1-dars/react.js
+++ rtworks/1-dars/react.js
@@ -1,2 +1,19 @@
 // React 
-/// react bu bitta freamwork
+
+/// react bu JavaScriptnni freamworki xissoblanadi ;
+  
+/// React-bu tashqi foydalanuvchi interfeyslarini yaratish uchun ochiq kodli JavaScript kutubxonasi.
+
+/// u bir programming language emas, yo'q sintax-xam emas xammasi JAvaScriptni syntaxiga asoslangan ;
+
+/// JSX
+
+/// Bu JavaScript tilining JSX kengaytmasi.                                                                           |
+//// React-ga UI qanday ko'rinishi kerakligini tushuntirish kerak bo'lganda foydalanishni tavsiya etamiz.             |--------https://ru.reactjs.org/docs/introducing-jsx.html
+///// JSX JavaScript kuchiga ega bo'lgan shablon tiliga o'xshaydi.                                                    |
+
+/// single page application :
+
+//// tarjimasi : bitta sahifali dastur ;
+
+///// uni nega bunday atalgan desa : chunki xamma react dastur pagelarimiz bitta HTML-faylga yig'iladi ;
