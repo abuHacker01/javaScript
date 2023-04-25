294e7bdee5ae0e4365d4fb98305da4c7317e5a38
abuhacker01 <abuhacker012@gmail.com>
2023-04-25 08:00:05 +0500

    works

--- JavaScript/20-dars/homework/index.js
+++ JavaScript/20-dars/homework/index.js
@@ -1,7 +1,7 @@
 let box = document.querySelector('.box');
 let ball = document.querySelector("#ball");
 
-box.addEventListener("mousemove", (event) => {
+box.addEventListener("click", (event) => {
     ball.style.marginTop = `${event.y - 55}px`;
     ball.style.marginLeft = `${event.x - 65}px`;
 });
